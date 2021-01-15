a38cda808e41f5fbd796b27d93322b155bc90b72
渡邊丈洋 <takehiro.watanabe@d2cdot.co.jp>
2021-01-15 16:12:27 +0900

    Initial Commit

---
+++ PhotoPrint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoPrint
+//
+//  Created by 渡邊 丈洋 on 2021/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
